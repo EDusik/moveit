cb3ea22474f5afd32b4d736fbb3531b8476b51ea
EDusik <eduardodusik@gmail.com>
2021-02-23 20:22:58 -0300

    styles: adding countdownButton

--- moveit-next/src/pages/_document.tsx
+++ moveit-next/src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
-          <title>NLW #4</title>
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
         </Head>
         <body>
           <Main />
